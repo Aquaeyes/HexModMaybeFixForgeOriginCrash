b24db86e6a4f040a31b58431b7924f180da3826a
Aquaholic <rjja27@hotmail.com>
2024-11-26 12:13:56 +0800

    use minecraft methods not awt for hsb->rgb

--- Common/src/main/java/at/petrak/hexcasting/common/items/pigment/ItemUUIDPigment.java
+++ Common/src/main/java/at/petrak/hexcasting/common/items/pigment/ItemUUIDPigment.java
@@ -10,7 +10,8 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.phys.Vec3;
 
-import java.awt.*;
+//import java.awt.*;
+import net.minecraft.util.Mth;
 import java.util.Random;
 import java.util.UUID;
 
@@ -59,8 +60,8 @@ public class ItemUUIDPigment extends Item implements PigmentItem {
             var saturation2 = rand.nextFloat(0.7f, 1.0f);
             var brightness2 = rand.nextFloat(0.2f, 0.7f);
 
-            var col1 = Color.HSBtoRGB(hue1, saturation1, brightness1);
-            var col2 = Color.HSBtoRGB(hue2, saturation2, brightness2);
+            var col1 = Mth.hsvToRgb(hue1, saturation1, brightness1);
+            var col2 = Mth.hsvToRgb(hue2, saturation2, brightness2);
             this.colors = new int[]{col1, col2};
         }
 
